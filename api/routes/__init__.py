15b52db7de7c1f750c4d2a4ea44d52b71e8c935a
laban254 <labanrotich6544@gmail.com>
2024-10-25 23:18:01 +0300

    Add cart and checkout routes

--- api/routes/__init__.py
+++ api/routes/__init__.py
@@ -1,9 +1,13 @@
 from fastapi import APIRouter
 from .auth import auth
 from .product import product_router
+from .cart import cart
+from .checkout import checkout_t
 
 api_version_one = APIRouter(prefix="/api/v1")
 
 
 api_version_one.include_router(auth)
 api_version_one.include_router(product_router)
+api_version_one.include_router(cart)
+api_version_one.include_router(checkout_t)
