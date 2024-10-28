a43b5169e739702c964d17a3f53285e4ce9f4bd0
laban254 <labanrotich6544@gmail.com>
2024-10-28 22:33:13 +0300

    Update models and add new tables

--- api/models/__init__.py
+++ api/models/__init__.py
@@ -3,3 +3,7 @@ from .product import Product
 from .cart import Cart
 from .order import Order
 from .order_item import OrderItem
+from .coupon import Coupon
+from .review import Review
+from .token_login import TokenLogin
+from .wishlist import Wishlist
